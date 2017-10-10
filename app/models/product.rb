545276f1354b893560d70bd00c2eb8f9c2fa678d
Ricardo Silva <riksilvap@gmail.com>
2017-10-10 12:28:51 -0300

    Custom fae index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   include Fae::BaseModelConcern
-
+  validates :title, uniqueness: true, presence: true
+  validates :price, presence: true
+  
   def fae_display_field
     title
   end
--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,8 +1,15 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+  validates :client, presence: true
+  
+  enum status: { finished: 0, canceled: 1 }
 
   def fae_display_field
-    
+    id
+  end
+
+  def self.for_fae_index
+    order(:id)
   end
 
 
